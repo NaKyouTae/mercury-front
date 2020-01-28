ab0442c0b56398719e0078a664c9f6d00f400087
nakyoutae <qppk@naver.com>
2020-01-28 22:50:37 +0900

    ing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'competition-angular';
+
+  constructor(private router: Router){}
+
+  ngOnInit(){
+    this.router.events.subscribe((e) => {
+      if(e instanceof NavigationEnd){
+        const height = window.innerHeight - document.querySelector('.page-header').clientHeight - document.querySelector('.page-footer').clientHeight - document.querySelector('.page-route').clientHeight;
+        const contentMinHeight = document.querySelector('.app-advert-one').clientHeight + 60;
+        document.querySelector('.page-content').setAttribute('style', 'height:' + height + 'px; min-height:' + contentMinHeight + 'px;');
+      }      
+    })
+  }
 }
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -12,4 +12,14 @@ export class LayoutComponent implements OnInit {
   ngOnInit() {
   }
 
+  ngAfterViewInit(){
+    this.resize();
+  }
+
+  resize(){
+    const height = window.innerHeight - document.querySelector('.page-header').clientHeight - document.querySelector('.page-footer').clientHeight - document.querySelector('.page-route').clientHeight;
+    const contentMinHeight = document.querySelector('.app-advert-one').clientHeight + 60;
+    document.querySelector('.page-content').setAttribute('style', 'height:' + height + 'px; min-height:' + contentMinHeight + 'px;');
+
+  }
 }

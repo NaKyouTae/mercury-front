da348126c6b3e21fce03be5bc7c3b84c70db8e80
나규태 <kyoutae@ybtour.co.kr>
2020-04-29 13:36:43 +0900

    jwt interceptor ing

--- src/app/core/interceptors/jwt/jwt-interceptor.service.ts
+++ src/app/core/interceptors/jwt/jwt-interceptor.service.ts
@@ -1,57 +1,55 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { HttpRequest, HttpHandler, HttpEvent, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { CookieService } from '../../../shared/common/cookie/cookies.service';
 import { JwtService } from 'src/app/shared/common/jwt/jwt.service';
-import * as moment from 'moment/moment';
 import { CommonHttpService } from 'src/app/shared/common/common-http.service';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class JwtInterceptorService {
-  constructor(private cookie: CookieService, private jwt: JwtService, private common: CommonHttpService) {}
+  constructor(private cookie: CookieService, private jwt: JwtService, private common: CommonHttpService) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    let Access = this.cookie.getCookie('Access-JWT');
+    const Access = this.cookie.getCookie('Access-JWT');
     const Refresh = this.cookie.getCookie('Refresh-JWT');
 
     if (Access !== null && Refresh !== null) {
-      const accessIsExpired = this.isTokenExpired(this.jwt.getJWTAccessKey('exp'));
-      const refreshIsExpired = this.isTokenExpired(this.jwt.getJWTRefreshKey('exp'));
-
-      if (refreshIsExpired && accessIsExpired) {
-        this.common.httpCallGet('service/tokens', Refresh).subscribe((res: any) => {
-          Access = res.result;
-
-          req = req.clone({
-            setHeaders: {
-              'Access-JWT': Access,
-              'Refresh-JWT': Refresh,
-            },
-          });
-        });
-      } else {
-        req = req.clone({
-          setHeaders: {
-            'Access-JWT': Access,
-            'Refresh-JWT': Refresh,
-          },
-        });
-      }
+      req = req.clone({
+        setHeaders: {
+          'Access-JWT': Access,
+          'Refresh-JWT': Refresh,
+        },
+      });
     }
 
-    return next.handle(req);
+    return next.handle(req).pipe(
+      map((event: HttpEvent<any>) => {
+        if (event instanceof HttpResponse) {
+          // do stuff with response and headers you want
+          if (event.body.resultCode === 'OK' && event.body.result !== null) {
+
+            const accessIsExpired = this.isTokenExpired(this.jwt.getJWTAccessKey('exp'));
+            const refreshIsExpired = this.isTokenExpired(this.jwt.getJWTRefreshKey('exp'));
+
+            if (!accessIsExpired) {
+              this.cookie.setCookie('Access-JWT', event.headers.get('Access-JWT'));
+            }
+
+            if (!refreshIsExpired) {
+              this.cookie.setCookie('Refresh-JWT', event.headers.get('Refresh-JWT'));
+            }
+          }
+        }
+        return event;
+      }));
   }
 
-  isTokenExpired(token: any): boolean {
-    if (!token) {
-      return true;
-    }
-
-    const date = new Date(0).setUTCSeconds(token.exp);
-
-    if (date === undefined) {
+  isTokenExpired(exp: any): boolean {
+    if (exp === null) {
       return false;
     }
-    return !(date.valueOf() > new Date().valueOf());
+
+    return !(Date.now() >= exp * 1000);
   }
 }

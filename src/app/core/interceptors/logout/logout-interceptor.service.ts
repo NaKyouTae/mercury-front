a8026c0a3916ed19772dd170a5cdf443ea704e3c
나규태 <kyoutae@ybtour.co.kr>
2020-04-29 16:32:43 +0900

    add

--- src/app/core/interceptors/logout/logout-interceptor.service.ts
+++ src/app/core/interceptors/logout/logout-interceptor.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CookieService } from 'src/app/shared/common/cookie/cookies.service';
-import { HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { HttpRequest, HttpHandler, HttpEvent, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
@@ -10,8 +10,17 @@ export class LogoutInterceptorService {
   constructor(private cookie: CookieService) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    if (req.url === 'http://localhost:8080/user/logout') {
-    }
-    return next.handle(req);
+    return next.handle(req).pipe(
+      map((event: HttpEvent<any>) => {
+        if (event instanceof HttpResponse) {
+          // do stuff with response and headers you want
+          if (event.body.resultCode === 'OK' && event.body.result !== null && event.url === 'http://localhost:8080/user/logout') {
+            this.cookie.deleteCookie('Access-JWT');
+            this.cookie.deleteCookie('Refresh-JWT');
+          }
+        }
+        return event;
+      })
+    );
   }
 }

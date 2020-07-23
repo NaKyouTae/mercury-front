ec8c7ef58317ffaa1f7af84de2217a82f3153b97
nakyoutae <qppk@naver.com>
2020-07-23 23:06:50 +0900

    batch fron update

--- src/app/pages/admin/batch/batch.component.ts
+++ src/app/pages/admin/batch/batch.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonHttpService } from 'src/app/shared/common/http/common-http.service';
 
 @Component({
   selector: 'app-batch',
   templateUrl: './batch.component.html',
-  styleUrls: ['./batch.component.css']
+  styleUrls: ['./batch.component.css'],
 })
 export class BatchComponent implements OnInit {
   public data: any;
   public fields: any;
 
-  constructor() { }
+  constructor(private common: CommonHttpService) {}
 
   ngOnInit() {
+    this.onSearchScheduler();
   }
 
+  public onSearchScheduler() {}
+
+  public onSearchTrigger() {}
+
+  public onSearchJob() {}
 }

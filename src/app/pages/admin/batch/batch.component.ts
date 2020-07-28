54d67e6503c03a74bb8eec2ec0888a9d1689c4aa
나규태 <kyoutae@ybtour.co.kr>
2020-07-28 17:35:23 +0900

    add

--- src/app/pages/admin/batch/batch.component.ts
+++ src/app/pages/admin/batch/batch.component.ts
@@ -10,7 +10,7 @@ export class BatchComponent implements OnInit {
   public data: any;
   public fields: any;
 
-  constructor(private common: CommonHttpService) {}
+  constructor(private common: CommonHttpService) { }
 
   ngOnInit() {
     this.onSearchScheduler();
@@ -24,7 +24,7 @@ export class BatchComponent implements OnInit {
           { title: 'Scheduler Name', width: 20, field: 'name' },
           { title: 'Trigger Index', width: 30, field: 'triggerIdx' },
           { title: 'Job Index', width: 20, field: 'jobIdx' },
-          { title: "Ins' Date", width: 20, field: 'insertDate' },
+          { title: 'Ins\' Date', width: 20, field: 'insertDate' },
         ];
         this.data = res.result;
       }
@@ -42,7 +42,7 @@ export class BatchComponent implements OnInit {
           { title: 'Trigger Title', width: 20, field: 'title' },
           { title: 'Desc', width: 20, field: 'description' },
           { title: 'Job Name', width: 20, field: 'jobTitle' },
-          { title: "Ins' Date", width: 20, field: 'insertDate' },
+          { title: 'Ins\' Date', width: 20, field: 'insertDate' },
         ];
       }
     });
@@ -57,7 +57,7 @@ export class BatchComponent implements OnInit {
           { title: 'Job Title', width: 20, field: 'title' },
           { title: 'Class Name', width: 30, field: 'clasz' },
           { title: 'Desc', width: 20, field: 'description' },
-          { title: "Ins' Date", width: 20, field: 'insertDate' },
+          { title: 'Ins\' Date', width: 20, field: 'insertDate' },
         ];
         this.data = res.result;
       }

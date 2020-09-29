f9d421e9e118aaa851cc48d6910cd314614d353b
나규태 <kyoutae@ybtour.co.kr>
2020-09-29 09:13:42 +0900

    kakao logout ing

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -14,9 +14,9 @@ export class HeaderComponent implements OnInit {
   public userCheck: any = this.user !== null ? true : false;
   public userName: any = this.user !== null ? this.user.username : '사용자';
   public loginType: any = this.jwt.getJWTUserKey('user') === null ? null : this.jwt.getJWTUserKey('user').sns;
-  constructor(private router: Router, private jwt: JwtService, private common: CommonHttpService, private cookie: CookieService) {}
+  constructor(private router: Router, private jwt: JwtService, private common: CommonHttpService, private cookie: CookieService) { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
   public logout() {
     if (window.confirm('로그아웃 하시겠습니까?')) {
@@ -33,12 +33,16 @@ export class HeaderComponent implements OnInit {
 
   public kakaoByLogout() {
     if (window.confirm('로그아웃 하시겠습니까?')) {
-      this.common.httpCallGet('user/kakao/logout', { acess: this.cookie.getCookie('AWT') }).subscribe((res: any) => {
-        if (res.resultCode === 'OK') {
-          this.router.navigateByUrl('/three');
-          window.location.reload();
-        }
-      });
+
+      // tslint:disable-next-line: max-line-length
+      window.location.href = 'http://kauth/kakao.com/oauth/logout?client_id=c4d7328a864db7fd90be93def8e00940&logout_redirect_uri=http://localhost:4300/user/kakao/logout&state=access_token'
+
+      // this.common.httpCallGet('user/kakao/logout', { acess: this.cookie.getCookie('AWT') }).subscribe((res: any) => {
+      //   if (res.resultCode === 'OK') {
+      //     this.router.navigateByUrl('/three');
+      //     window.location.reload();
+      //   }
+      // });
     } else {
       return false;
     }

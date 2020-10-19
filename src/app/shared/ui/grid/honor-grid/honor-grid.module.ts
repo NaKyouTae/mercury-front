0ec91b7488ecbd3757fce9eaeacbde768bba136d
nakyoutae <qppk@naver.com>
2020-10-19 23:41:07 +0900

    honor grid ing

--- src/app/shared/ui/grid/honor-grid/honor-grid.module.ts
+++ src/app/shared/ui/grid/honor-grid/honor-grid.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HonorGridComponent } from './honor-grid.component';
 import { RowDbclickModule } from 'src/app/shared/directive/grid-details/row-dbclick/row-dbclick.module';
 import { InfiniteScrollSelectModule } from '../../select/infinite-scroll-select/infinite-scroll-select.module';
+import { BtnClickModule } from 'src/app/shared/directive/grid-details/btn-click/btn-click.module';
 
 @NgModule({
   declarations: [HonorGridComponent],
   exports: [HonorGridComponent],
-  imports: [CommonModule, RowDbclickModule, InfiniteScrollSelectModule],
+  imports: [CommonModule, RowDbclickModule, BtnClickModule, InfiniteScrollSelectModule],
 })
-export class HonorGridModule { }
+export class HonorGridModule {}
